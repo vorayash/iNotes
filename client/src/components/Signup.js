d8f91817ce0a8464136a8ebec7c24afe8df3ff8c
vorayash <vorayash9028@gmail.com>
2022-02-14 18:59:38 +0530

    make it better

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -14,7 +14,7 @@ const Signup = () => {
     const {name,email,password} = credential;
     const handleSubmit=async(e)=>{
         e.preventDefault();
-        const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+        const response = await fetch(`/api/auth/createuser`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
