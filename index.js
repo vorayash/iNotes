bda170cbc1128e0b9343d18e7e70435e394f8a93
vorayash <vorayash9028@gmail.com>
2022-02-14 18:43:44 +0530

    make it better

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.use('/api/notes', require('./routes/notes'))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static( 'client/build' ));
 
-  // app.get('*', (req, res) => {
-  //     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-  // });
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+  });
 }
 
 app.listen(port, () => {
